7af194dfebdce1a1210d6bf8447bd177be149313
Gianluca Ielpo <126248252+ielpo@users.noreply.github.com>
2026-03-17 20:22:17 +0100

    Fix newline issue when combining files

--- doc/adr/compile-pdf.sh
+++ doc/adr/compile-pdf.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 echo "Combining all ADRs into a single PDF..."
-cat *.md > combined.md
+
+for file in *.md; do
+    cat "$file" >> "combined.md"
+    echo -e "\n" >> "combined.md"
+done
+
 pandoc combined.md -o adrs.pdf
 rm "combined.md"
